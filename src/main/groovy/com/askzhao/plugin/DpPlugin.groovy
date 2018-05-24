78658fe9b71625a5b1341f547795a95d3b94010e
zhaochuang <zhaochuang@finupgroup.com>
2018-05-24 14:47:37 +0800

    增加异常

--- src/main/groovy/com/askzhao/plugin/DpPlugin.groovy
+++ src/main/groovy/com/askzhao/plugin/DpPlugin.groovy
@@ -17,7 +17,7 @@ public class DpPlugin implements Plugin<Project> {
 
         project.dependencies {
             compile 'org.aspectj:aspectjrt:1.8.10'
-            compile 'com.github.askzhao:dp-runtime:1.0.3'
+            compile 'com.github.askzhao:dp-runtime:1.0.4'
         }
 
         project.extensions.create("askDataPoint", AskDataPointExtension)
